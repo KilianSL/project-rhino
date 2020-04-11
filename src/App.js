6dfd2b76b4ba3bd0a7987afa4f6815b9ae79b782
KilianSL <seifk007.319@wcgs.foliotrust.uk>
2020-04-11 15:40:22 +0100

    Built basic file infrastructure

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
-import Rhino from './Rhino';
+import Rhino from './components/app/Rhino';
 
 function App() {
   return (
---
+++ src/components/general/NotFound.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const style = {
+    paddingTop : "100px;",
+    textAlign : "center"
+};
+
+export default () => 
+    <div style={style}>
+        <h3>Sorry, this page doesn't exist!</h3>
+    </div>;
