95ec113b2f6023d2368ae35eb1ce328dcaeeee89
KilianSL <seifk007.319@wcgs.foliotrust.uk>
2020-04-14 23:38:44 +0100

    Started styling mobile view

--- src/components/app/AppRoutes.tsx
+++ src/components/app/AppRoutes.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import {Route, Redirect} from 'react-router-dom';
 import Rhino from './Rhino';
+import Home from './Home';
 
 export default function AppRoutes(props:object){
     return(
         <>
-        <Route path='/' exact component={Rhino} />
+        <Route path='/' exact component={Home} />
+        <Route path='/contribute' exact component={Rhino} />
         </>
     )
 }
