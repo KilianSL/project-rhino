2d0ede3f1945d59f12a403c2df2eb422a5c7b11c
KilianSL <seifk007.319@wcgs.foliotrust.uk>
2020-04-15 22:23:02 +0100

    Build mobile-scaled landing page component

--- src/components/app/LandingPage.tsx
+++ src/components/app/LandingPage.tsx
@@ -1,89 +1,13 @@
 import React from 'react';
 import {jsx, css} from '@emotion/core';
 /**@jsx jsx */
-import {Button, useMediaQuery, SlideProps} from '@material-ui/core';
+import {Button, useMediaQuery} from '@material-ui/core';
 import {Link} from 'react-router-dom';
+import '../../classes.css';
 
 // Responsive landing page component - breaks at 1000px
 // Shows either slides displaying text+button or paragraphs of text for MobileStepper
 // Last slide is form to sign up / enter any details
-
-const style = css`
-    padding: 25px;
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    justify-content: center;
-    box-sizing: border-box;
-    color: #42494f;
-    font-size: 16px;
-
-    div{
-        max-width: 800px;
-        width: 100%;
-        box-sizing: border-box;
-        display: block;
-        font-weight: 500;
-        margin: auto;
-    }
-
-    h1, h2{
-        color: 1a1a1a;
-        font-weight: 300;
-    }
-    h1{
-        font-size: 48px;
-        line-height: 58px;
-        display: block;
-        margin-block-start: 0.67em;
-        margin-block-end: 0.67em;
-        margin-inline-start: 0px;
-        margin-inline-end: 0px;
-    }
-    h3{
-        padding-bottom: 50px;
-        padding-top: 20px;
-        margin: auto;
-        max-width: 435px;
-        font-size: 24px;
-        line-height: 34px;
-        font-weight: 300;
-        display: block;
-        margin-block-start: 1em;
-        margin-block-end: 1em;
-        margin-inline-start: 0px;
-        margin-inline-end: 0px;
-        color: #42494f;
-    }
-    h2{
-        text-align: center;
-        color: #1a1a1a;
-        margin: auto;
-        max-width: 600px;
-        font-size: 30px;
-        line-height: 40px;
-        font-weight: 300 !important;
-        margin-block-start: 0.83em;
-        margin-block-end: 0.83em;
-        margin-inline-start: 0px;
-        margin-inline-end: 0px;
-        display: block;
-    }
-    h6{
-        padding-top: 30px;
-        padding-bottom: 30px;
-        margin: 0;
-        font-size: 18px;
-        line-height: 28px;
-        font-weight: 500;
-        box-sizing: border-box;
-        display: block;
-        margin-block-start: 2.33em;
-        margin-block-end: 2.33em;
-        margin-inline-start: 0px;
-        margin-inline-end: 0px;
-    }
-`
 
 type LandingPageSlideProps = {
     title : string,
@@ -93,27 +17,58 @@ type LandingPageSlideProps = {
 type LandingPageProps = {
     title : string, // Title displayed at top of all pages
     subtitle : string, // Subtitle + content of first slide
+    logo : string,
     slides : Array<LandingPageSlideProps>, // List of slides to display
     btnText : string, // Text to be on button of each slide
     btnLink : React.FunctionComponent // Link for the button to go to
 }
 
+const style = css`
+    padding: 25px;
+
+    div {
+        display: block;
+    }
+
+    .logo{
+        padding-bottom: 50px;
+    }
+
+    h1{
+        margin: 0;
+    }
+
+    h3{
+        padding-bottom: 50px;
+        padding-top: 20px;
+
+    }
+
+    h6{
+        padding-top: 30px;
+        padding-bottom: 30px;
+        margin: 0;
+        text-align: left;
+    }
+`
+
 export default function LandingPage(props : LandingPageProps){
     return(
         // mOBILE VERIOSN
-        <div css={style}> 
-            <div className="title">
-                <h1>{props.title}</h1>
-                <h3>{props.subtitle}</h3>
+        <div className={"fl-column fl-center"} css={style}> 
+            <div className="txt-center logo">
+                <img src={props.logo} />
+            </div>
+            <div className="w-full w-max-800 title">
+                <h1 className="txt-center">{props.title}</h1>
+                <h3 className="w-max-435 txt-center m-auto">{props.subtitle}</h3>
             </div>
             {props.slides.map(
                 (slide) => {
                     return(
-                        <div className="paragraph">
-                            <h2>{slide.title}</h2>
+                        <div className="paragraph w-full w-max-800">
+                            <h2 className="m-auto txt-center w-max-600">{slide.title}</h2>
                             <h6>{slide.content}</h6>
-                            <br />
-                            <br />
                         </div>
                     )
                 }
