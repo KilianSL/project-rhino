5f66b9cb33f719fd78ea1843b5ae41fac670c78e
KilianSL <seifk007.319@wcgs.foliotrust.uk>
2020-04-20 23:32:51 +0100

    Considered using background image. Still lost on material ui added a pages folder for the user-facing pages

--- src/components/LandingPage/LandingPage.tsx
+++ src/components/LandingPage/LandingPage.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
-import {useMediaQuery} from '@material-ui/core';
+import {useMediaQuery, Container, Grid, Button} from '@material-ui/core';
+import {Link} from 'react-router-dom';
 
 // Landing page for browser and mobile. 
 // Displays a title and subtitle over a background image, 
@@ -9,20 +10,62 @@ type props = {
     title : string,
     subtitle : string,
     logo : string,
+    background : string,
     button1 : {
         text : string,
         to : string
-    }
+    },
     button2 : {
         text : string,
         to : string
-    }
+    },
     footer : string
 }
 
 export default function LandingPage(props : props){
 
     const isMobile = useMediaQuery('(max-width:1000px)');
-    
+
+    return(
+        <div style={{width:"100vw", 
+            height : "100vh", 
+            overflow: "hidden",
+            backgroundImage : `url(${props.background})`,
+            backgroundSize : "cover" }}>
+            <Container maxWidth="lg">
+                <Grid
+                    container
+                    direction="column"
+                    justify="flex-start"
+                    alignItems="center"
+                >
+                    <img src={props.logo} />
+                    <h1>{props.title}</h1>
+                    <h3>{props.subtitle}</h3>
+                    <Grid 
+                        item
+                        direction="row"
+                        justify="space-evenly"
+                    >
+                        <Button 
+                            color="primary"
+                            component={Link}
+                            to={props.button1.to}
+                        >
+                            {props.button1.text}
+                        </Button>
+                        <Button 
+                            color="secondary"
+                            component={Link}
+                            to={props.button2.to}
+                        >
+                            {props.button2.text}
+                        </Button>
+                    </Grid>
+                </Grid>
+                <footer style={{bottom: "0"}}>The AI Core</footer>
+            </Container>
+        </div> 
+    )
 }
 
